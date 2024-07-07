c8f06a949feb8f88561e3b8175c5417a097b6667
Aymen-Khanfir <aymenkhanfir56@gmail.com>
2024-07-07 14:48:02 +0100

    Adding a user image picker widget

--- lib/screens/chat.dart
+++ lib/screens/chat.dart
@@ -8,7 +8,7 @@ class ChatScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('CHATAK'),
+        title: const Text('Welcome'),
         actions: [
           IconButton(
             onPressed: () {
